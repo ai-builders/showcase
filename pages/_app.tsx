c2f11fc0bfa285ac76925d3e65f046d7e62cad9d
tulakann <tulakan.r@gmail.com>
2022-04-14 00:38:11 +0700

    build: add basePath /showcase to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import "@fontsource/bai-jamjuree";
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import '@fontsource/bai-jamjuree';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.css';
 
 const theme = extendTheme({
   fonts: {
@@ -16,14 +16,14 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="##5bbad5" />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/showcase/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/showcase/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/showcase/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/showcase/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/showcase/favicon/safari-pinned-tab.svg" color="##5bbad5" />
+        <link rel="shortcut icon" href="/showcase/favicon/favicon.ico" />
         <meta name="msapplication-TileColor" content="##5bbad5" />
-        <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+        <meta name="msapplication-config" content="/showcase/favicon/browserconfig.xml" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
       <ChakraProvider theme={theme}>
